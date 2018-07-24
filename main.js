b6c3cd5b211c106f27a8d2cd63e7c0de0896a4c5
surajmishra1007 <surajmishra1007@gmail.com>
2018-07-24 13:38:16 +0530

    Assignment 7.1

---
+++ main.js
@@ -0,0 +1,14 @@
+var num1 = -8; //variable declaration
+var num2 = 12.5; //variable declaration
+var num3 = -7.729; //variable declaration
+var num4 = 0; //variable declaration
+var num5 = -1; //variable declaration
+
+var max = num1;
+
+if (num2 > max) max = num2; //Conditions
+if (num3 > max) max = num3;
+if (num4 > max) max = num4;
+if (num5 > max) max = num5;
+
+console.log("max no. is " + max); //Output
